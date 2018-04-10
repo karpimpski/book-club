60e1c40cd6b1f7d9146948e1e4eaac2b943ef349
= <=>
2018-04-10 13:20:41 -0400

    basic navbar

--- app/routes.js
+++ app/routes.js
@@ -1,14 +1,14 @@
 module.exports = function(app, passport) {
 	app.get("/", function(req, res) {
-		res.render("index.ejs")
+		res.render("index.ejs", {user: req.user})
 	})
 
 	app.get("/login", function(req, res) {
-		res.render("login.ejs", { message: req.flash("loginMessage") })
+		res.render("login.ejs", { message: req.flash("loginMessage"), user: req.user })
 	})
 
 	app.get("/signup", function(req, res) {
-		res.render("signup.ejs", { message: req.flash("signupMessage") })
+		res.render("signup.ejs", { message: req.flash("signupMessage"), user: req.user })
 	})
 
 	app.get("/profile", isLoggedIn, function(req, res) {
